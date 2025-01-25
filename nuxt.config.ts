9e9d4294e2a6c181882cdfdd9f70ac0082e18dd1
amer-wild11 <amer33wild@gmail.com>
2025-01-25 19:43:56 +0300

    Test version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@prisma/nuxt", "@nuxt/icon", "@nuxtjs/netlify"],
+  modules: ["@pinia/nuxt", "@prisma/nuxt", "@nuxt/icon"],
   css: ["~/assets/scss/main.scss"],
-  ssr: true,  // تأكد من تفعيل SSR
+  ssr: true, // تأكد من تفعيل SSR
   nitro: {
     preset: "netlify", // استخدام Netlify كإعداد مسبق يمكن أن يعمل مع Render كذلك
   },
